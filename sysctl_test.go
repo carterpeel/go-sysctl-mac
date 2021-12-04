21e1fbacdc97585bcf3bb88e7929284fb09940c2
carterpeel <carterpeel@gmail.com>
2021-12-04 11:57:55 -0500

    Added tests

--- sysctl_test.go
+++ sysctl_test.go
@@ -1,20 +1,32 @@
-package tests
+package sysctl
 
 import (
-	gosysctlmac "github.com/carterpeel/go-sysctl-mac"
 	"testing"
 )
 
 func TestPersistent(t *testing.T) {
-	// SetPersistent activates the config option now, but also adds it to
-	// /private/etc/sysctl.conf so it persists over reboots.
-	if err := gosysctlmac.SetPersistent("net.inet.ip.forwarding", "1"); err != nil {
+	t.Cleanup(func() {
+		if err := Set("net.inet.ip.forwarding", "0"); err != nil {
+			t.Fatalf("error disabling IP forwarding: %v\n", err)
+		}
+	})
+
+	// SetPersistent() activates the config option now, but also adds it to
+	// /private/etc/sysctl.conf, so it persists over reboots.
+	if err := SetPersistent("net.inet.ip.forwarding", "1"); err != nil {
 		t.Fatalf("error enabling IP forwarding: %v\n", err)
 	}
 }
 
 func TestNonPersistent(t *testing.T) {
-	if err := sysctl.Set("kern.maxproc", "4096"); err != nil {
-		t.Fatalf("error disabling IP forwarding: %v\n", err)
+	t.Cleanup(func() {
+		if err := Set("kern.maxproc", "2048"); err != nil {
+			t.Fatalf("error resetting maxprocs to default: %v\n", err)
+		}
+	})
+
+	// Set() sets a config option that will not persist upon reboot.
+	if err := Set("kern.maxproc", "4096"); err != nil {
+		t.Fatalf("error incrementing maxproc: %v\n", err)
 	}
 }
